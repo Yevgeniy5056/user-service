394a3835b2c32966ece89105e89c8c7996747ad0
Geodakyan Yevgeniy <yevgengeodakyan@gmail.com>
2024-08-27 20:47:09 -0400

    environment to consume api ready

---
+++ src/main/java/com/cydeo/client/ProjectClient.java
@@ -0,0 +1,17 @@
+package com.cydeo.client;
+
+import com.cydeo.dto.ProjectResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "project-service", url = "localhost:8082")
+public interface ProjectClient {
+
+    @GetMapping("api/v1/project/count/manager/{assignedManager}")
+    ResponseEntity<ProjectResponse> getNonCompletedCountByAssignedManager(
+            @PathVariable("assignedManager") String assignedManager);
+
+
+}
