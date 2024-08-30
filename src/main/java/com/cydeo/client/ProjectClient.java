805f8d0a24a2fae87a59d43c7a78237ebf538077
Geodakyan Yevgeniy <yevgengeodakyan@gmail.com>
2024-08-30 09:45:36 -0400

    made that service a Eureka client

--- src/main/java/com/cydeo/client/ProjectClient.java
+++ src/main/java/com/cydeo/client/ProjectClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "project-service", url = "localhost:8082")
+@FeignClient(name = "project-service")
 public interface ProjectClient {
 
     @GetMapping("api/v1/project/count/manager/{assignedManager}")
--- src/main/java/com/cydeo/client/TaskClient.java
+++ src/main/java/com/cydeo/client/TaskClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "task-service", url = "localhost:8083")
+@FeignClient(name = "task-service")
 public interface TaskClient {
 
     @GetMapping("api/v1/task/count/employee/{assignedEmployee}")
