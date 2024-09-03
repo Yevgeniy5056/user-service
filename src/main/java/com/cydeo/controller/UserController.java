8dd11d3019c2a9503d50548334ca8c0423f6fca4
Geodakyan Yevgeniy <yevgengeodakyan@gmail.com>
2024-09-03 09:07:54 -0400

    cloud config client added

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +21,7 @@ import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
 import java.util.List;
 
+@RefreshScope
 @CrossOrigin(origins = "http://localhost:8762")
 @RestController
 @RequestMapping("/api/v1/user")
